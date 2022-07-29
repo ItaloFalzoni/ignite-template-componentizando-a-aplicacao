3afa8489bc161d32fcb56caac099599828dd6849
Ítalo Falzoni <italo.falzoni@gmail.com>
2022-07-29 07:22:06 -0300

    first commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App'
+import '../styles/sidebar.scss'
+import { Button } from './Button'
+
+interface Props {
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  handleClick: (id: number) => void
+}
+
+export function SideBar({ genres, selectedGenreId, handleClick }: Props) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
